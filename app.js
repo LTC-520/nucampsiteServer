b529be2151ee3cf793c0d9b1a2bce85ba4fad37c
LTC-520 <ltcodes@duck.com>
2022-05-23 18:10:53 -0700

    updated function, res and req with session

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const connect = mongoose.connect(url, {
   useUnifiedTopology: true
 });
 
+const session = require(express-session);
+const FileStore = require('session-file-store')(session);
+
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
@@ -32,10 +35,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false, // when a new session is created & not updates are done, it won't be saved at the end of the sessions
+  resave: false, //once a session is created, updated and saved, it will continue to be resaved whenever a request is made to the session
+  store: new FileStore() //creates a new file store as an object which saves the sessions information to a hard disk instead of the apps memory
+}))
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session)
+  if (!req.session.user) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
       const err = new Error('you are not authenticated');
@@ -47,7 +59,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user', 'admin', { signed: true })
+      res.session.user = 'admin'; //saving this session that the username is "admin"
       return next(); //authorized
     } else {
       const err = new Error('You are not authenticated');
@@ -56,7 +68,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
