f468e3d974dc4f9017a5a94477bc6a97a358b0b9
LTC-520 <ltcodes@duck.com>
2022-05-24 13:53:20 -0700

    updated app

--- app.js
+++ app.js
@@ -43,32 +43,19 @@ app.use(session({
   saveUninitialized: false, // when a new session is created & not updates are done, it won't be saved at the end of the sessions
   resave: false, //once a session is created, updated and saved, it will continue to be resaved whenever a request is made to the session
   store: new FileStore() //creates a new file store as an object which saves the sessions information to a hard disk instead of the apps memory
-}))
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
   console.log(req.session)
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
       const err = new Error('you are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
-    }
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin'; //saving this session that the username is "admin"
-      return next(); //authorized
-    } else {
-      const err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       return next();
     } else {
       const err = new Error('You are not authenticated!');
@@ -82,8 +69,7 @@ app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
